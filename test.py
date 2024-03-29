55781cd3a7d8113380886b697280e446826d9674
golangboy <golangboy@golangboydeMacBook-Air.local>
2024-03-29 20:51:53 +0800

    fix

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ from ultralytics import YOLO
 if __name__ =='__main__':
     # Load a model
     model = YOLO('./ultralytics/cfg/models/v8/yolov8dcn.yaml')  # load a pretrained model (recommended for training)
+    print(model)
+    model.train("coco-pose.yaml",epochs=10)
