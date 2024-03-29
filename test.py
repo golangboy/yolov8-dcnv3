e179163fe703c61f949379d80ec0843b43b4ad98
golangboy <golangboy@golangboydeMacBook-Air.local>
2024-03-29 20:11:56 +0800

    add

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ =='__main__':
     # Load a model
-    model = YOLO('ultralytics/ultralytics/cfg/models/v8/yolov8dcn.yaml')  # load a pretrained model (recommended for training)
+    model = YOLO('./ultralytics/cfg/models/v8/yolov8dcn.yaml')  # load a pretrained model (recommended for training)
