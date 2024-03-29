0c06c6e5f75722caf812d1e5c8483fc5e1a945d8
golangboy <golangboy@golangboydeMacBook-Air.local>
2024-03-29 19:54:03 +0800

    fix

---
+++ test.py
@@ -0,0 +1,6 @@
+import DCNv3
+from ultralytics import YOLO
+
+if __name__ =='__main__':
+    # Load a model
+    model = YOLO('ultralytics/ultralytics/cfg/models/v8/yolov8dcn.yaml')  # load a pretrained model (recommended for training)
